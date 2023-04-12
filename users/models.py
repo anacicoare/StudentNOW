fc3c5fd8e733e50e184a762a8b19cefe561a1297
Ana Cicoare <anacicoare03@gmail.com>
2023-04-13 01:36:07 +0300

    added hackitall initial project files

---
+++ users/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
+
+
+# Extending User Model Using a One-To-One Link
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    points = 0
+    
+    avatar = models.ImageField(default='default.jpg', upload_to='profile_images')
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.user.username
+
+    # resizing images
+    def save(self, *args, **kwargs):
+        super().save()
+
+        img = Image.open(self.avatar.path)
+
+        if img.height > 100 or img.width > 100:
+            new_img = (100, 100)
+            img.thumbnail(new_img)
+            img.save(self.avatar.path)
+
+class Student(models.Model):
+    name = models.CharField(max_length=200)
+    points = models.IntegerField()
+    email = models.CharField(max_length=40)
+    def __str__(self):
+        return self.name
